c1f5ec1aebb60c68e65508aca34ef502f88f1e45
Alexey Khabirov <finko85@gmail.com>
2020-02-26 00:44:27 +0300

    installed react router, create offer details component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainPage from "../main-page/main-page.jsx";
+import OfferDetail from '../offer-detail/offer-detail.jsx';
 
-const App = (props) => {
-  const {data} = props;
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <MainPage data={data}/>
-  );
-};
+  render() {
+    const {data} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <MainPage data={data}/>
+          </Route>
+          <Route exact path="/offers-detail">
+            <OfferDetail/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
