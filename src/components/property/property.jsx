fadae7e476897a9d6949c4629cdfeaa070838861
Alex Khab <finko85@gmail.com>
2020-03-04 00:10:15 +0300

    adds reviews-list ans review component

--- src/components/property/property.jsx
+++ src/components/property/property.jsx
@@ -1,42 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Offer from '../offer/offer.jsx';
+import ReviewsList from '../reviews-list/reviews-list.jsx';
 
-const Property = ({offer}) => {
+const Property = ({offer, reviews}) => {
   return (
     <React.Fragment>
-      <Offer offer={offer}></Offer>
+      <Offer offer={offer}/>
       <section className="property__reviews reviews">
-        <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-        <ul className="reviews__list">
-          <li className="reviews__item">
-            <div className="reviews__user user">
-              <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54"
-                  alt="Reviews avatar"/>
-              </div>
-              <span className="reviews__user-name">
-                Max
-              </span>
-            </div>
-            <div className="reviews__info">
-              <div className="reviews__rating rating">
-                <div className="reviews__stars rating__stars">
-                  <span style={{width: `80%`}}/>
-                  <span className="visually-hidden">Rating</span>
-                </div>
-              </div>
-              <p className="reviews__text">
-                A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is
-                green and from 18th century.
-              </p>
-              <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
-            </div>
-          </li>
-        </ul>
+        <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
+        <ReviewsList reviews={reviews}/>
       </section>
     </React.Fragment>
   );
+};
+
+Property.propTypes = {
+  offer: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    promoImage: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    isPremium: PropTypes.bool.isRequired,
+    roomsCount: PropTypes.number.isRequired,
+    guestsCount: PropTypes.number.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    features: PropTypes.arrayOf(PropTypes.string).isRequired,
+    owner: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      description: PropTypes.arrayOf(PropTypes.string).isRequired,
+      isPro: PropTypes.bool.isRequired
+    }).isRequired,
+  }).isRequired,
+  reviews: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    date: PropTypes.number.isRequired,
+  })).isRequired,
 };
 
 export default Property;
