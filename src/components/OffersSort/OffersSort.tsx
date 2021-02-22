fa8f5d1c2b18681e97d83d752c1115194f23fe37
Alexey Khabirov <Khabirov.A@citilink.ru>
2021-02-22 11:18:55 +0300

    feat: add useClickOutside

--- src/components/OffersSort/OffersSort.tsx
+++ src/components/OffersSort/OffersSort.tsx
@@ -1,36 +1,45 @@
 import cx from 'classnames';
 import { nanoid } from 'nanoid';
-import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useCallback, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { SortType } from '../../const';
-import {changeSortType, selectSortType } from '../../features/ui/uiSlice';
-import { useToggle } from '../../hooks/useToggle';
+import { changeSortType, selectSortType } from '../../features/ui/uiSlice';
+import { useOnClickOutside } from '../../hooks/useOutsideClick';
 
 
 const OffersSorting: React.FC = () => {
-  const [isSelectOpen, toggleSelect] = useToggle();
+  const selectRef = useRef(null);
+  const [isSelectOpen, toggleSelect] = useState(false);
 
   const dispatch = useDispatch();
   const activeSortOption = useSelector(selectSortType);
 
+  const handleClick = () => toggleSelect(!isSelectOpen);
+
   const handleSortOptionClick = (option: string) => {
-    toggleSelect();
+    toggleSelect(!isSelectOpen);
     dispatch(changeSortType(option));
   };
+
+  const handleClickOutside = useCallback(() => {
+    toggleSelect(false);
+  }, []);
+
+  useOnClickOutside(selectRef, handleClickOutside);
 
   const selectCls = cx('places__options', 'places__options--custom', {
     'places__options--opened': isSelectOpen,
   });
 
   return (
-    <form className="places__sorting" action="#" method="get">
+    <form className="places__sorting" action="#" method="get" ref={selectRef}>
       <span className="places__sorting-caption">Sort by</span>
 
       <span
         className="places__sorting-type"
         tabIndex={0}
-        onClick={toggleSelect}
+        onClick={handleClick}
       >
         {activeSortOption}
         <svg className="places__sorting-arrow" width="7" height="4">
